b660a5e10d301957da2b554506560f406058aca1
wukunyun <864353360@qq.com>
2024-07-24 20:18:10 +0800

    inject

--- AnnotationProcessor/AnnotationProcessor.java
+++ AnnotationProcessor/AnnotationProcessor.java
@@ -1,36 +1,35 @@
-import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
-@interface MyAnnotation {
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@interface MyAnnotation{
     String value();
+    int id();
 }
 
-public class AnnotationProcessor {
-    public static void main(String[] args) throws Exception {
-        // 获取类
-        Class<MyClass> myClass = MyClass.class;
-        // 处理字段上的注解
-        for (Field field : myClass.getDeclaredFields()) {
-            if (field.isAnnotationPresent(MyAnnotation.class)) {
-                MyAnnotation annotation = field.getAnnotation(MyAnnotation.class);
-                System.out.println("Field: " + field.getName() + ", Annotation value: " + annotation.value());
-            }
-        }
-        // 处理方法上的注解
-        for (Method method : myClass.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(MyAnnotation.class)) {
-                MyAnnotation annotation = method.getAnnotation(MyAnnotation.class);
-                System.out.println("Method: " + method.getName() + ", Annotation value: " + annotation.value());
-            }
-        }
+class Test{
+    @MyAnnotation(value = "Parker", id =1)
+    String name;
+    @MyAnnotation(value = "byd", id =2)
+    String realName;
+
+    @MyAnnotation(value = "Bier", id =3)
+    void sayMyName(String name){
+        System.out.println(name);
     }
 }
-class MyClass {
-    @MyAnnotation(value = "Field Annotation")
-    private String myField;
-    @MyAnnotation(value = "Method Annotation")
-    public void myMethod() {
-        // 方法体
+
+public class AnnotationProcessor{
+    public static void main(String[] args) {
+            Class<Test> test = Test.class;
+            for (Field field : test.getDeclaredFields()){
+                if(field.isAnnotationPresent(MyAnnotation.class)){
+                    System.out.println(field.getName() + "&& id: " +  field.getAnnotation(MyAnnotation.class).id() + " && name: " + field.getAnnotation(MyAnnotation.class).value());
+                }
+            }
     }
 }
